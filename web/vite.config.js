6781acbe47c3dd00a903b4bf4334bc1b9cc0cbe6
nextjs-wedding <geeksinbyte@gmail.com>
2025-06-10 00:24:59 +0700

    injectregister

--- web/vite.config.js
+++ web/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: false,
+      injectRegister: true,
 
       pwaAssets: {
         disabled: false,
